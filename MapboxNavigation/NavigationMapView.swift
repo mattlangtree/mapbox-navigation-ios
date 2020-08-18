d31b6324b50bc7fbd3b209ebc233186218b7326b
Maxim Makhun <maxim.makhun@mapbox.com>
2020-08-18 16:16:42 -0700

    Add ability to re-add routes, waypoints and highlighted buildings in NavigationMapView when style was changed.

--- MapboxNavigation/NavigationMapView.swift
+++ MapboxNavigation/NavigationMapView.swift
@@ -135,7 +135,8 @@ open class NavigationMapView: MGLMapView, UIGestureRecognizerDelegate {
     var altitude: CLLocationDistance
     var routes: [Route]?
     var isAnimatingToOverheadMode = false
-
+    var styleObservation: NSKeyValueObservation?
+    
     /**
      A tuple that represents the dictionary of percentage/color pairs used to
      calculate the color gradient transitions belonging to the main route line
@@ -143,6 +144,11 @@ open class NavigationMapView: MGLMapView, UIGestureRecognizerDelegate {
      */
     typealias RouteGradientStops = (line: [CGFloat: UIColor], casing: [CGFloat: UIColor])
     private var routeGradientStops = RouteGradientStops(line: [:], casing: [:])
+    
+    /**
+     A tuple that represents currently highlighted buildings and their extrusion (either 2D or 3D).
+     */
+    private var highlightedBuildings: (coordinates: [CLLocationCoordinate2D], extrudesBuildings: Bool)?
     
     var shouldPositionCourseViewFrameByFrame = false {
         didSet {
@@ -256,6 +262,10 @@ open class NavigationMapView: MGLMapView, UIGestureRecognizerDelegate {
         commonInit()
     }
     
+    deinit {
+        styleObservation = nil
+    }
+    
     fileprivate func commonInit() {
         makeGestureRecognizersRespectCourseTracking()
         makeGestureRecognizersUpdateCourseView()
@@ -266,6 +276,19 @@ open class NavigationMapView: MGLMapView, UIGestureRecognizerDelegate {
         addGestureRecognizer(mapTapGesture)
         
         installUserCourseView()
+        
+        styleObservation = observe(\.style, options: .new) { [weak self] (mapView, change) in
+            guard let self = self else { return }
+            
+            if let routes = self.routes, let currentRoute = routes.first {
+                self.show(routes)
+                self.showWaypoints(on: currentRoute)
+            }
+            
+            if let highlightedBuildings = self.highlightedBuildings {
+                self.highlightBuildings(at: highlightedBuildings.0, in3D: highlightedBuildings.1)
+            }
+        }
     }
     
     open override func layoutMarginsDidChange() {
@@ -1358,6 +1381,7 @@ extension NavigationMapView {
      - parameter extrudesBuildings: Switch which allows to highlight buildings in either 2D or 3D. Defaults to true.
      */
     public func highlightBuildings(at coordinates: [CLLocationCoordinate2D], in3D extrudesBuildings: Bool = true) {
+        highlightedBuildings = (coordinates, extrudesBuildings)
         highlightBuildings(with: Set(coordinates.compactMap({ buildingIdentifier(at: $0) })), in3D: extrudesBuildings)
     }
     
@@ -1365,6 +1389,7 @@ extension NavigationMapView {
      Removes the highlight from all buildings highlighted by `highlightBuildings(at:in3D:)`.
      */
     public func unhighlightBuildings() {
+        highlightedBuildings = nil
         guard let highlightedBuildingsLayer = style?.layer(withIdentifier: StyleLayerIdentifier.buildingExtrusion) else { return }
         
         style?.removeLayer(highlightedBuildingsLayer)
